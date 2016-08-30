1fb37a636dc3b030cc3160f56faf62cf4ec11553
Neil <vitalene@msu.edu>
2016-08-30 16:30:36 -0400

    Initial Commit

---
+++ TextPlay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextPlay
+//
+//  Created by Neil Vitale on 8/30/16.
+//  Copyright © 2016 Neil Vitale. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
